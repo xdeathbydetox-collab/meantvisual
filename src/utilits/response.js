25a579db1e3cfa07a6899658b7d4d42c46a2855f
xdeathbydetox-collab <xdeathbydetox@gmail.com>
2026-08-29 15:20:02 +0300

    Update response.js

--- src/utilits/response.js
+++ src/utilits/response.js
@@ -1,1 +1,32 @@
+export function json(data, status = 200, extraHeaders = {}) {
+  return new Response(JSON.stringify(data), {
+    status,
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": "true",
+      ...extraHeaders
+    }
+  });
+}
 
+export function optionsResponse() {
+  return new Response(null, {
+    status: 204,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods":
+        "GET, POST, PUT, PATCH, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers":
+        "Content-Type, Accept, Authorization",
+      "Access-Control-Allow-Credentials": "true"
+    }
+  });
+}
+
+export function notFound() {
+  return json({
+    success: false,
+    error: "Маршрут не найден"
+  }, 404);
+}
